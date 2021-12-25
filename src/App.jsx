0d43a6fbc27fc36c8584d2cedf4f63d376ec17cd
nkwaaaa <nkwaaaa@gmail.com>
2021-12-25 17:32:00 -0300

    agregada api the movie db

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 export function App() {
     return (
-        <div>
+        <Router>
             <header>
-                <h1 className={styles.title}>Movies</h1>
+                <Link to="/">
+                    <h1 className={styles.title}>Movies</h1>
+                </Link>
             </header>
 
             <main>
-                <MoviesGrid />
+                <Switch>
+                    <Route exact path="/movies/:movieId">
+                        <MovieDetails />
+                    </Route>
+                    <Route path="/">
+                        <LandingPage />
+                    </Route>
+                </Switch>
             </main>
-        </div>
+        </Router>
     );
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import styles from "./MovieCard.module.css";
+
+export function MovieCard({ movie }) {
+    console.log(styles);
+
+    const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+    return (
+        <li className={styles.movieCard}>
+            <Link to={"/movies/" + movie.id}>
+                <img
+                    width={230}
+                    height={345}
+                    className={styles.movieImage}
+                    src={imageUrl}
+                    alt={movie.title}
+                />
+                {movie.title}
+            </Link>
+        </li>
+    );
+}
