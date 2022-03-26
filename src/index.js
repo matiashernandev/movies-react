20d9e54747cf89a9ec72146fd0fa76e78b048da8
nkwaaaa <73903008+nkwaaaa@users.noreply.github.com>
2022-03-26 02:27:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App.jsx";
 
-/* const contenido = (
-    <div>
-        <h1>Este es el título</h1>
-        <div>Hola desde reactTTTT </div>
-    </div>
-); */
-
-/* function Componente({ titulo, contenido, children }) {
-    // console.log(props);
-    // let { titulo, contenido } = props;
-
-    return (
-        <div className="contenedor">
-            <h1>{titulo}</h1>
-            <div>{contenido} </div>
-            <div>{children} </div>
-        </div>
-    );
-} */
 
 ReactDOM.render(
-    /* <Componente titulo="c mata" contenido="Hola mundo llorando">
-        {" "}
-        Este es el children{" "}
-    </Componente> */
+ 
 
     <App />,
     document.getElementById("root")
